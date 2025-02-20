19c5afb8412a75564e7a7063645a1484a96a6a63
sergiogarcia0050 <segarcia@stone.com.co>
2025-02-20 08:02:20 -0500

    feat: :sparkles: adding the name of models

--- knowledge_suppliers/models/knowledgeSupplier.py
+++ knowledge_suppliers/models/knowledgeSupplier.py
@@ -1,9 +1,22 @@
+import uuid
 from django.db import models
 
 from courses.models.course import Course
 
 class KnowledgeSupplier(models.Models):
-	id = models.UUIDField(primary_key = True)
-	name = models.TextField(max_length = 100)
+	id = models.UUIDField(primary_key = True, default=uuid.uuid4)
+	name = models.TextField(max_length = 100, unique = True)
 	addres = models.TextField(max_length = 100)
 	course_id = models.ForeignKey(Course, on_delete=models.PROTECT)
+
+
+	class Meta:
+		db_table = 'knowledge_supplier'
+		verbose_name = 'knowledge_sipplier'	
+		verbose_name_plural = 'knowledge_sippliers'
+
+
+
+	
+
+	
--- people/models/people.py
+++ people/models/people.py
@@ -1,9 +1,15 @@
+import uuid
 from django.db import models
 
 
 class Person(models.Model):
-	id = models.UUIDField(primary_key=True, editable=False)
+	id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
 	name = models.CharField(max_length=100)
+
+	class Meta:
+		db_table = 'person'
+		verbose_name = 'person'	
+		verbose_name_plural = 'persons'
 	
 
 	
