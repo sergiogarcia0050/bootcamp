749591826621d93a1ee211c80093f2a86c5b0660
Juan Sebastian Gomez <156712607+jsgomezs1@users.noreply.github.com>
2025-02-21 07:10:26 -0500

    primer endpoint

--- courses/urls.py
+++ courses/urls.py
@@ -2,3 +2,11 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from courses.views.course import CourseViewSet
 
+
+router = DefaultRouter()
+
+router.register(r'courses', CourseViewSet)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
