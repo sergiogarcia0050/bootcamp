3d8dce0de242a5e3593cc84e93e605724cbc330b
Juan Sebastian Gomez <156712607+jsgomezs1@users.noreply.github.com>
2025-02-21 07:48:14 -0500

    my first migration

--- courses/tests/course.py
+++ courses/tests/course.py
@@ -1,5 +1,15 @@
 from rest_framework.test import APITestCase
 
+from courses.models.course import Course
+from people.models.people import Person
+
 class CourseTestCase(APITestCase):
     
+    def setUp(self):
+        self.person = Person.objects.create(name = 'El Joe Arroyo')
+        self.course = Course.objects.create(name= 'Salsa', descripcion = "Un gran curso de salsa")
+        
+    def test_create_course(self):
+        
+        
     
--- events/models/event.py
+++ events/models/event.py
@@ -1,12 +1,19 @@
 from django.db import models
-
 from courses.models.batch import Batch
 from courses.models.course import Course
+
 class Event(models.Model):
     id = models.UUIDField(primary_key=True)
     name = models.CharField(max_length=100)
     description = models.TextField()
-    type = models.Choices('I', 'P', 'F') # I = Inicial, P = Progreso, F = Finalizacion
+    
+    CATEGORY_CHOICES = [
+        ('I', 'Inicial'),
+        ('P', 'Progreso'),
+        ('F', 'Finalización'),
+    ]
+    
+    category = models.CharField(max_length=1, choices=CATEGORY_CHOICES)
     start_date = models.DateField()
     end_date = models.DateField()
     course = models.ForeignKey(Course, null=True, on_delete=models.PROTECT)
@@ -19,7 +26,7 @@ class Event(models.Model):
 
         constraints = [
             models.CheckConstraint(
-                check=models.Q(start_date__lt = models.F('end_date')),
+                check=models.Q(start_date__lt=models.F('end_date')),
                 name='start_date_must_be_less_than_end_date'
             ),
             models.CheckConstraint(
@@ -30,15 +37,11 @@ class Event(models.Model):
                 name='exactly_one_batch_or_bootcamp'
             ),
             models.CheckConstraint(
-                check = models.Q(batch__isnull=False) & models.Q(type__in=['I', 'F']),
+                check=models.Q(batch__isnull=False) & models.Q(category__in=['I', 'F']),
                 name='initial_and_final_events_must_have_batch'
             ),
             models.CheckConstraint(
-                check = (models.Q(course__isnull=False) & models.Q(type_in=['I', 'P','F'])),
+                check=models.Q(course__isnull=False) & models.Q(category__in=['I', 'P', 'F']),
                 name='course_events_can_only_be_initial_progress_or_final'
             )
-
         ]
-
-    
-
